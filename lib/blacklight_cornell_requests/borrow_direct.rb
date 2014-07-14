10f433bf9d499c1d2ec7e31acbcebfd6eae7348f
Matthew Connolly <mjc12@cornell.edu>
2014-07-14 14:48:55 -0400

    Rewrite borrow direct library to use zoom gem and Yaz toolkit instead of pazpar2 server.

--- lib/blacklight_cornell_requests/borrow_direct.rb
+++ lib/blacklight_cornell_requests/borrow_direct.rb
@@ -1,17 +1,15 @@
+require 'zoom'
+
 module BlacklightCornellRequests
 
   module BorrowDirect
+
     # Main function to call. This exists mostly to wrap the meat of the pazpar2 querying within a begin/rescue block
     # params = { :isbn, :title } - the two parameters that we're using to query Borrow Direct availabiilty.
     # ISBN is best, but title will work if ISBN isn't available.
     def borrowDirect_available? params
 
-      begin
-        return _borrowDirect_available? params
-      rescue => e
-        Rails.logger.info "Error checking borrow direct availability: exception #{e.class.name} : #{e.message}"
-        return false
-      end
+      return _borrowDirect_available? params
 
     end
 
@@ -53,97 +51,94 @@ module BlacklightCornellRequests
         return false
       end
 
-      base_url = _bd_url params[:env_http_host]
-
-      session_id = _initialize_session base_url
-      # TODO: Matt start here
-      # Rails.logger.info "session id: #{session_id}"
-
-      ## make pazpar2 search
       isbn = params[:isbn]
       if isbn.present?
         isbn = /([a-zA-Z0-9]+)/.match(params[:isbn][0])
         isbn = isbn[1]
       end
-      # isbn = params[:isbn][0].scan(/"([a-zA-Z0-9]+)[ "]/)
-      # logger.info "isbn:"
-      # logger.info isbn.inspect
+
+      search_params = nil
       if isbn.blank? && !params[:title].blank?
-        request_url = base_url + "/search.pz2?session=#{session_id}&command=search&query=ti%3D" + ERB::Util.url_encode("\"#{params[:title]}\"")
+        # Do a title search
+        search_params = "@attr 1=4 #{params[:title].gsub(' ', '+')}"
       elsif !isbn.blank?
-        request_url = base_url + "/search.pz2?session=#{session_id}&command=search&query=isbn%3D#{isbn}"
+        # Do an isbn search
+        search_params = "@attr 1=7 #{isbn}}"
       else
         return false
       end
-      # Rails.logger.info "request url: #{request_url}"
-      response = HTTPClient.get_content(request_url)
-      response_parsed = Hash.from_xml(response)
-      status = response_parsed['search']['status']
-      # Rails.logger.info "bd response: " + response_parsed.inspect
-      if status != 'OK'
-        ## invalid search
-        logger.info "Invalid search: #{status}"
-        return false
-      end
 
-      ## get pazpar2 recid from show command to get record information
-      ## make stat request repeatedly to check if the search process finished
-      sleep(0.5)
-      i = 0
-      progress = '0.00'
-      request_url = base_url + "/search.pz2?session=#{session_id}&command=stat"
-      while (progress != '1.00' && i < 120)
-        response = HTTPClient.get_content(request_url)
-        response_parsed = Hash.from_xml(response)
-        progress = response_parsed['stat']['progress']
-        i = i + 1
-        sleep(1)
-      end
-      # logger.info "finished search request in #{i} seconds"
-      ## make show request to get record id
-      request_url = base_url + "/search.pz2?session=#{session_id}&command=show&start=0&sort=title:1"
-      response = HTTPClient.get_content(request_url)
-      response_parsed = Hash.from_xml(response)
-      hits = response_parsed['show']['hit']
-      if hits.blank? || hits.class == String
-        return false
-      elsif hits.class == Hash
-        return _determine_availablility? base_url, session_id, hits
-      elsif hits.class == Array
-        hits.each do |hit|
-          return true if _determine_availablility? base_url, session_id, hit
+      servers = [
+        ['josiah.brown.edu', 210, 'INNOPAC'],   # 'available'
+        ['clio-db.cc.columbia.edu', 7090, 'voyager'], # 'circulations'
+        ['catalog-lib.dartmouth.edu', 210, 'innopac'], # 'available'
+        ['catalog.lib.jhu.edu', 210, 'ipac'],     # 'circulations'
+        ['library.mit.edu', 9909, 'mit01pub'],      # NIL?!
+        ['catalog.princeton.edu', 7090, 'voyager'], # 'circulations'
+        ['libcat.uchicago.edu', 210, 'uofc'],   # 'circulations'
+        ['z3950.franklin.library.upenn.edu', 7090, 'voyager'], #'circulations'
+        ['prodorbis.library.yale.edu', 7090, 'voyager'] #'circulations'
+      ]
+
+      available = false
+
+      # Main z39.50 search loop. For each library server in the list, 
+      # make a connection and search by isbn or title as appropriate.
+      # As soon as we find a positive result for availability, break
+      # out and return true so the system can get on with things.
+      # NOTE: this method only checks general availability, not 
+      # actual Borrow Direct availability. This will give us some 
+      # false positives, but, if we're lucky, not nearly as many
+      # false positives as the number of false negatives we're currently
+      # seeing because pazpar2 simply isn't returning results for 
+      # many of these servers!
+      servers.each do |s|
+  Rails.logger.warn "mjc12test: Activating groovy search for #{s[0]}"
+
+        begin
+
+          ZOOM::Connection.open(s[0], s[1]) do |conn|
+
+            conn.database_name = s[2]
+            conn.preferred_record_syntax = 'opac'
+            results = conn.search(search_params)
+            #Rails.logger.warn "mjc12test: results: #{results[0].inspect}"
+            results_xml = Nokogiri::XML(results[0].to_s)
+
+            # innopac servers indicate availability in the publicNote field;
+            # all others in the list (all voyager?) use 'availableNow'
+            if s[2] == 'INNOPAC' or s[2] == 'innopac'
+              holdings = results_xml.xpath('//holdings/holding/publicNote')
+              holdings.each do |h|
+                available = (h.content == 'AVAILABLE')
+                if available
+                  Rails.logger.warn "mjc12test: available from #{s[0]}"
+                end
+             #   return true if available
+              end
+            else
+              holdings =results_xml.xpath('//holdings/holding/circulations/circulation/availableNow')
+              holdings.each do |h|
+                available = (h.attribute('value').to_s == '1')
+                            if available
+                  Rails.logger.warn "mjc12test: available from #{s[0]}"
+                end
+             #   return true if available
+              end
+            end
+
+          end # zoom do
+
+        rescue => e
+          Rails.logger.info "Error checking borrow direct availability: exception #{e.class.name} : #{e.message}"
         end
-      else
-        ## error?
-      end
 
-      ## get record for each hit returned until we find first available item or there is no more
-      return false
-    end
+        # return true if available
 
-    def _determine_availablility? borrow_direct_webservices_url, session_id, hit
-      recid = hit['recid']
-      request_url = borrow_direct_webservices_url + "/search.pz2?session=#{session_id}&command=record&id=#{recid}"
-      response = HTTPClient.get_content(URI::escape(request_url))
-      response_parsed = Hash.from_xml(response)
-      availabilities = response_parsed['record']['location']['md_available']
-      if availabilities.class == String
-        if availabilities.strip == 'Available'
-          return true
-        end
-      elsif availabilities.class == Array
-        availabilities.each do |availability|
-          if availability.strip == 'Available'
-            return true
-          end
-        end
-      else
-        ## what is this?
-        # logger.debug availabilities.inspect
-        return false
-      end
-    end
+      end # servers.each
 
-  end
+    end # def borrowdirect_available?
+
+  end # module
 
 end
