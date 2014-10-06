df46a9451711b084794113ea3b4eb3ac8dea39df
Matthew Connolly <mjc12@cornell.edu>
2014-10-06 12:01:19 -0400

    Sanitize holdings VCR files for dotenv.

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -4,4 +4,7 @@ VCR.configure do |c|
   # your HTTP request service. You can also use fakeweb, typhoeus, and more
   c.hook_into :webmock
   c.allow_http_connections_when_no_cassette = true # This means that we don't *always* have to use VCR for HTTP, only when we want
+  c.filter_sensitive_data('<REST_URL>')  { "#{ENV['REST_URL']}"}
+  c.filter_sensitive_data('<VOYAGER_DB>') { "#{ENV['VOYAGER_DB']}"}
+  c.filter_sensitive_data('<DUMMY_VOYAGER_HOLDINGS>') { "#{ENV['DUMMY_VOYAGER_HOLDINGS']}"}
 end
