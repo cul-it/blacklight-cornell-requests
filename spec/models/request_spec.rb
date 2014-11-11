131215d04b0fcc2d5f5bc0db321257bab04650bf
Matthew Connolly <mjc12@cornell.edu>
2014-11-11 16:24:24 -0500

    Add rspec tests for guest delivery options.

--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -56,7 +56,7 @@ describe BlacklightCornellRequests::Request do
         end
       end
 
-    end
+    end #describe populate document values
 
     describe 'get_delivery_time' do
 
@@ -259,7 +259,7 @@ describe BlacklightCornellRequests::Request do
 
       end
 
-    end
+    end #describe get delivery time
 
     describe 'sort_request_options' do 
 
@@ -271,13 +271,110 @@ describe BlacklightCornellRequests::Request do
         expect(sorted_options).to eq([{:estimate=>[1, 1]}, {:estimate=>[3, 5]}, {:estimate=>[4, 6]}])
       end
 
-    end
+    end #describe sort request options
 
 
 
 
-  end
+  end # describe secondary functions
 
+  describe "Primary functions" do
 
+    describe "get_guest_delivery_options" do
 
-end
+      let (:request)  { FactoryGirl.create(:request) }
+
+      context "noncirculating item" do
+        it "returns an empty set for 'nocirc' items" do
+          item = { :item_type_id => 9 }
+          options = request.get_guest_delivery_options(item)
+          expect(options).to eq([])
+        end
+        it "return an empty set for other noncirculating items" do
+          item = { :item_type_id => 2, :status => 1 }
+          request.stub(:noncirculating?).and_return(true)
+          options = request.get_guest_delivery_options(item)
+          expect(options).to eq([])
+        end
+      end  # context noncirculating item
+
+      context "regular loan item" do
+        it "returns L2L if item is not charged" do
+          item = { :item_type_id => 2, :status => 1 }
+          options = request.get_guest_delivery_options(item)
+          expect(options[0][:service]).to eq('l2l')
+          expect(options.count).to eq(1)
+        end
+        it "returns HOLD if item is charged" do
+          item = { :item_type_id => 2, :status => 2 }
+          options = request.get_guest_delivery_options(item)
+          expect(options[0][:service]).to eq('hold')
+          expect(options.count).to eq(1)
+        end
+        it "returns an empty set if the item is missing" do
+          item = { :item_type_id => 2, :status => 12 }
+          options = request.get_guest_delivery_options(item)
+          expect(options).to eq([])    
+        end
+        it "returns an empty set if the item is lost" do
+          item = { :item_type_id => 2, :status => 26 }
+          options = request.get_guest_delivery_options(item)
+          expect(options).to eq([])     
+        end
+      end # context regular loan item
+
+      context "day loan item" do 
+        it "returns L2L if item is not charged" do
+          item = { :item_type_id => 5, :status => 1 }
+          options = request.get_guest_delivery_options(item)
+          expect(options[0][:service]).to eq('l2l')
+          expect(options.count).to eq(1)        
+        end
+        it "returns HOLD if item is charged" do
+          item = { :item_type_id => 5, :status => 2 }
+          options = request.get_guest_delivery_options(item)
+          expect(options[0][:service]).to eq('hold')
+          expect(options.count).to eq(1)
+        end
+        it "returns an empty set if the item is missing" do
+          item = { :item_type_id => 5, :status => 12 }
+          options = request.get_guest_delivery_options(item)
+          expect(options).to eq([])  
+        end
+        it "returns an empty set if the item is lost" do
+          item = { :item_type_id => 5, :status => 26 }
+          options = request.get_guest_delivery_options(item)
+          expect(options).to eq([]) 
+        end
+      end # context day loan item
+
+      context "minute loan item" do 
+        it "returns ask at circulation if item is not charged" do
+          item = { :item_type_id => 12, :status => 1 }
+          options = request.get_guest_delivery_options(item)
+          expect(options[0][:service]).to eq('circ')
+          expect(options.count).to eq(1) 
+        end
+        it "returns ask at circulation if item is charged" do
+          item = { :item_type_id => 12, :status => 2 }
+          options = request.get_guest_delivery_options(item)
+          expect(options[0][:service]).to eq('circ')
+          expect(options.count).to eq(1)        
+        end
+        it "returns an empty set if the item is missing" do
+          item = { :item_type_id => 12, :status => 12 }
+          options = request.get_guest_delivery_options(item)
+          expect(options).to eq([])          
+        end
+        it "returns an empty set if the item is lost" do
+          item = { :item_type_id => 12, :status => 26 }
+          options = request.get_guest_delivery_options(item)
+          expect(options).to eq([])          
+        end
+      end # context day loan item
+
+    end #describe get guest delivery options
+
+  end #describe primary functions
+
+end #describe request
