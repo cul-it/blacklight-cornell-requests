a9e8c65f9d6a3fd6f96e855223143e2dacc81cd0
Matt Connolly <mjc12@cornell.edu>
2015-03-10 15:41:36 -0400

    Add tests for borrow direct library.

---
+++ spec/libraries/borrow_direct_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'blacklight_cornell_requests/borrow_direct'
+
+describe BlacklightCornellRequests::BorrowDirect do
+
+  describe "Primary availability function" do
+
+    it ""
+
+  end
+
+end
