8ecc821f0187129b7bccd513c858770ee7b8feb9
Rick Silterra <es287@cornell.edu>
2017-09-19 10:28:56 -0400

    I did not take the state of the test db into account

--- db/migrate/20170918143824_change_circ_policy_group.rb
+++ db/migrate/20170918143824_change_circ_policy_group.rb
@@ -1,8 +1,10 @@
 class ChangeCircPolicyGroup < ActiveRecord::Migration
   def change
     av = BlacklightCornellRequests::Circ_policy_locs.where(:LOCATION_ID => 216).first
-    av.delete
-    BlacklightCornellRequests::Circ_policy_locs.create(:CIRC_GROUP_ID => 5, :LOCATION_ID => 216, :PICKUP_LOCATION => 'N')
+    if !av.nil? 
+      av.delete
+    end
+    av = BlacklightCornellRequests::Circ_policy_locs.create(:CIRC_GROUP_ID => 5, :LOCATION_ID => 216, :PICKUP_LOCATION => 'N')
     av.save!
   end
 end
