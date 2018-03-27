0950d80749cb654ff6b8239ee65ff27ca158345b
Matt Connolly <mjc12@cornell.edu>
2018-03-27 16:09:00 -0400

    Improve R3 report

--- app/models/blacklight_cornell_requests/r3.rb
+++ app/models/blacklight_cornell_requests/r3.rb
@@ -19,20 +19,26 @@ module BlacklightCornellRequests
       # Items are keyed by the associated holding record
       holdings.each do |h, item_array|
         item_array.each do |i|
-          items << BlacklightCornellRequests::Item.new(h, i)
+          items << Item.new(h, i)
         end
       end
       items
     end
 
-    def report
+    def report(item_id = '')
       linebreak
       puts "Reporting on bib #{@bibid} for #{@requester.netid}\n\n"
+      if item_id.present?
+        @items = @items.select do |i|
+          i.id == item_id
+        end
+        puts "Focus on item #{item_id}"
+      end
       linebreak
       puts "Requester: #{@requester.netid} (barcode: #{@requester.barcode})\n"
       puts "\n\n\n"
       linebreak
-      puts "Delivery methods available:"
+      puts "Delivery methods available (i.e., not disabled in ENV file):"
       @methods.each do |m|
         puts "#{m.description}"
       end
@@ -44,10 +50,29 @@ module BlacklightCornellRequests
       puts "Voyager delivery methods valid for this item:"
       puts RequestPolicy.policy(@items[0].circ_group, @requester.patron_group, @items[0].type['id'])
       linebreak
+      puts "Individual item report:"
+      puts "ID\t\tStatus\tAvailable\tL2L\tHold\tRecall\n"
+      @items.each do |i|
+        rp = RequestPolicy.policy(i.circ_group, @requester.patron_group, i.type['id'])
+        puts "#{i.id}\t#{i.status['code'].keys[0]}\t#{i.status['available']}\t\t#{l2l_available?(i, rp)}\t#{hold_available?(i, rp)}\t#{recall_available?(i, rp)}"
+      end
+      linebreak
     end
 
     def linebreak
       puts "-" * 50
+    end
+
+    def l2l_available?(item, policy)
+      (L2L.enabled? && policy[:l2l] && item.status['available']) ? 'Y' : 'N'
+    end
+
+    def hold_available?(item, policy)
+      (Hold.enabled? && policy[:hold] && !item.status['available']) ? 'Y' : 'N'
+    end
+
+    def recall_available?(item, policy)
+      (Recall.enabled? && policy[:recall] && !item.status['available']) ? 'Y' : 'N'
     end
 
 
