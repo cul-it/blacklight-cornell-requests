99ea5e6d3ef4a9cb4caeb10cd5d5d65fc3b450b2
Matt Connolly <mjc12@cornell.edu>
2018-05-15 09:49:58 -0400

    Fix use of clean_isbn method

--- app/models/blacklight_cornell_requests/cul_borrow_direct.rb
+++ app/models/blacklight_cornell_requests/cul_borrow_direct.rb
@@ -137,16 +137,16 @@ module BlacklightCornellRequests
 
   end # class
 
-  class String
-    def clean_isbn
-      temp = self
-      if self.index(' ')
-        temp   = self[0,self.index(' ')]
-      end
-      temp =  temp.size == 10 ? temp : temp.gsub!(/[^0-9X]*/, '')
-      temp =  temp.size == 13 ? temp : temp.gsub!(/[^0-9X]*/, '')
-      temp
-    end
-  end
+end
 
+class String
+  def clean_isbn
+    temp = self
+    if self.index(' ')
+      temp   = self[0,self.index(' ')]
+    end
+    temp =  temp.size == 10 ? temp : temp.gsub!(/[^0-9X]*/, '')
+    temp =  temp.size == 13 ? temp : temp.gsub!(/[^0-9X]*/, '')
+    temp
+  end
 end
