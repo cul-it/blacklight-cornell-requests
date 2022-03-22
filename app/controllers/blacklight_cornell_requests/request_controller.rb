c48c103f9f638f4e54e8db8a5eb098c659c06a55
James Reidy <jreidy@cornell.edu>
2022-03-22 13:35:18 -0400

    DISCOVERYACCESS-7436 - remove debugging code

--- app/controllers/blacklight_cornell_requests/request_controller.rb
+++ app/controllers/blacklight_cornell_requests/request_controller.rb
@@ -33,38 +33,9 @@ module BlacklightCornellRequests
       if uri.port.present? && uri.port !=  uri.default_port()
         scheme_host = scheme_host + ':' + uri.port.to_s
       end
-#******************
-save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
-jgr25_context = "#{__FILE__}:#{__LINE__}"
-Rails.logger.warn "jgr25_log\n#{jgr25_context}:"
-msg = [" #{__method__} ".center(60,'Z')]
-msg << jgr25_context
-msg << "request.original_url: " + request.original_url.inspect
-msg << "uri: " + uri.inspect
-msg << 'Z' * 60
-msg.each { |x| puts 'ZZZ ' + x.to_yaml }
-Rails.logger.level = save_level
-#binding.pry
-#*******************
       id_format = params[:format].present? ? params[:bibid] + '.' + params[:format] : params[:bibid]
       session[:cuwebauth_return_path] = magic_request_path(id_format)
       Rails.logger.debug "es287_log #{__FILE__} #{__LINE__}: #{magic_request_path(id_format).inspect}"
-      Rails.logger.debug "es287_log #{__FILE__} #{__LINE__}: #{request.original_url.inspect}"
-#******************
-save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
-msg.each { |x| puts 'ZZZ ' + x.to_yaml }
-jgr25_context = "#{__FILE__}:#{__LINE__}"
-Rails.logger.warn "jgr25_log\n#{jgr25_context}:"
-msg = [" #{__method__} ".center(60,'Z')]
-msg << jgr25_context
-msg << "request.original_url: " + request.original_url.inspect
-msg << "uri: " + uri.inspect
-msg << "scheme_host: " + scheme_host.inspect
-msg << 'Z' * 60
-msg.each { |x| puts 'ZZZ ' + x.to_yaml }
-Rails.logger.level = save_level
-#binding.pry
-#*******************
       if ENV['DEBUG_USER'] && Rails.env.development?
         magic_request target
       else
