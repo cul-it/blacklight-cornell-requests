5a805276e6153f85e2719cdb94558ddedec29497
Matt Connolly <mjc12@cornell.edu>
2024-12-04 17:12:25 -0500

    Update RSpec (#287)
    * Remove outdated test components
    
    * Replace old dummy environment with new (dumber?) dummy
    
    * Update rakefile and gemspec
    
    * Update rspec setup
    
    * Try to get rspec to run
    
    * Fix rspec error
    
    * Get rspec to recognize models and load paths

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -1,9 +1,5 @@
-require 'rubygems'
-
 # Set up gems listed in the Gemfile.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../../Gemfile", __dir__)
 
-ENV['RAILS_ENV'] ||= IO.read('.env').match(/RAILS_ENV=(\w+)/)[1]
-
-
-require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+require "bundler/setup" if File.exist?(ENV["BUNDLE_GEMFILE"])
+$LOAD_PATH.unshift File.expand_path("../../../lib", __dir__)
