aec9592e1d8f9df5bc9d60a83efb6b68b74361cb
Matthew Connolly <mjc12@cornell.edu>
2025-12-03 11:49:47 -0500

    Use extended table name

--- db/migrate/20251203142805_drop_circ_policy_locs.rb
+++ db/migrate/20251203142805_drop_circ_policy_locs.rb
@@ -1,5 +1,5 @@
 class DropCircPolicyLocs < ActiveRecord::Migration[7.2]
   def change
-    drop_table :circ_policy_locs, if_exists: true
+    drop_table :blacklight_cornell_requests_circ_policy_locs, if_exists: true
   end
 end
