2543f9d216e439a5072358271d0fdeb77a953a74
Matthew Connolly <mjc12@cornell.edu>
2025-12-03 11:03:49 -0500

    Simplify migration

--- db/migrate/20251203142805_drop_circ_policy_locs.rb
+++ db/migrate/20251203142805_drop_circ_policy_locs.rb
@@ -1,9 +1,5 @@
 class DropCircPolicyLocs < ActiveRecord::Migration[7.2]
   def change
-    drop_table :circ_policy_locs do |t|
-      t.integer :CIRC_GROUP_ID
-      t.string  :PICKUP_LOCATION
-      t.integer :LOCATION_ID
-    end
+    drop_table :circ_policy_locs, if_exists: true
   end
 end
