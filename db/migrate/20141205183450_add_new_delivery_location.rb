12510377c0530614d315fa21a9776653a1255906
Matthew Connolly <mjc12@cornell.edu>
2025-12-04 10:01:37 -0500

    Comments

--- db/migrate/20141205183450_add_new_delivery_location.rb
+++ db/migrate/20141205183450_add_new_delivery_location.rb
@@ -1,4 +1,7 @@
 class AddNewDeliveryLocation < ActiveRecord::Migration[5.2]
   def change
+    # This space intentionally left blank
+    # (Keeping the migration so that schema versions stay consistent, but eliminating references to the 
+    # defunc Circ_policy_locs model)
   end
 end
